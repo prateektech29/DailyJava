71d984eba70c209d44309cec4289e26552a14555
prateektech29 <prateektech29@gmail.com>
2024-10-12 09:33:25 +0530

    exercises: Add Java practice files for topic Array

---
+++ src/JavaArrays/Array/LaunchAr1.java
@@ -0,0 +1,48 @@
+package JavaArrays.Array;
+
+import java.util.Scanner;
+
+public class LaunchAr1
+{
+	public static void main(String[] args) 
+	{
+		 int []marks=new int[995453699];
+		int []marks2=new int[2147483647];
+		
+
+		
+	
+		
+		//int l=marks.length;
+//		marks[0]=14;
+//		marks[1]=13;
+//		marks[2]=14;
+//		marks[2]=45.5;
+//		marks[3]=15;
+//		marks[4]=12;
+		
+		
+		
+		
+		//int m=marks[0];
+		//System.out.println(marks[0]);
+		
+//		Scanner scan=new Scanner(System.in);
+//		for(int i=0;i<marks.length;i++)
+//		{
+//			System.out.println("Kindly Enter the marks of student "+i);
+//			marks[i]=scan.nextInt();
+//		}
+//		
+//		System.out.println("Marks of Students stored in an array : ");
+//		for(int i=0;i<marks.length;i++)
+//		{
+//			System.out.print(marks[i]+ " ");
+//		}
+//		System.out.println();
+		
+		
+	
+	}
+
+}
---
+++ src/JavaArrays/Array/LaunchAr2.java
@@ -0,0 +1,60 @@
+package JavaArrays.Array;
+
+import java.util.Scanner;
+
+public class LaunchAr2 
+{
+
+	public static void main(String[] args) 
+	{
+//		int [][]marks=new int[3][4];
+		int [][]marks=new int[3][];
+		marks[0]=new int[4];
+		marks[1]=new int[2];
+		marks[2]=new int[3];
+		
+//		int[][] a = new int[3][3];
+//
+//		a[1]= new int[5];
+
+		
+//		Scanner scan=new Scanner(System.in);
+//
+//		for(int i=0;i<marks.length;i++)
+//		{
+//			for(int j=0;j<marks[i].length;j++)
+//			{
+//				System.out.println("Kindly enter marks of class "+ i + " Student "+ j);
+//				marks[i][j]=scan.nextInt();
+//			}
+//		}
+//		System.out.println("Marks stored in an array");
+//
+//
+//		for(int i=0;i<marks.length;i++)
+//		{
+//			for(int j=0;j<marks[i].length;j++)
+//			{
+//				System.out.print(marks[i][j]+ " ");
+//
+//			}
+//			System.out.println();
+//		}
+//
+//
+//		//int []ar= {12,2};
+//
+		Object ar[]=new Object[3];
+		ar[0]=23;
+		ar[1]="Alien";
+		ar[2]=45.4;
+
+		//		for(Object a: ar)
+//		{
+//			System.out.println(a);
+//		}
+
+
+	}
+
+}
